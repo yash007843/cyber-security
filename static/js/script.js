a854f2d7561fd4f9beabc2acfdc31fa44a681787
Somasundaram S <94049161+Somu0809@users.noreply.github.com>
2025-03-17 12:47:03 +0530

    render api url updated

--- static/js/script.js
+++ static/js/script.js
@@ -10,7 +10,7 @@ document.getElementById('predictForm').addEventListener('submit', function(event
     }
 
     // Send a POST request to the backend to predict toxicity
-    fetch('http://127.0.0.1:5000/predict', {
+    fetch('https://cyber-security-gb3e.onrender.com/predict', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
